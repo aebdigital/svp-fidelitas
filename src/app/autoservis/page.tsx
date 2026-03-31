d91582cca5745430ef87d4a10aa3adde65c5d14d
Alexander Hidvéghy <alexanderhidveghy@Mac.lan>
2026-03-31 12:38:25 +0200

    Enable optimized images for autoservis

--- src/app/autoservis/page.tsx
+++ src/app/autoservis/page.tsx
@@ -12,19 +12,16 @@ const sections: ServiceSidebarSection[] = [
     title: "Servisné služby pre vozidlá",
     description: "Ponúkame: opravy motorov, diagnostika vozidiel, príprava na STK, oprava podvozkových častí – tlmiče, brzdy, plnenie a kompletný servis AC, výmena čelných skiel, čistenie – leštenie skiel reflektorov vozidiel, čistenie klimatizácie ( ionizácia ), montáž ťažných zariadení, zápis servisu do RSZV",
     images: [{ src: "/images/autoservis/1.jpg", alt: "Servisné služby pre vozidlá" }],
-    unoptimized: true,
   },
   {
     title: "Predaj a výmena prevádzkových kvapalín pre vozidlá",
     description: "brzdová kvapalina, voda do ostrekovačov ( letná, zimná ), chladiaca zmes, prevodový olej, olejové, peľové, kabínové, vzduchové filtre",
     images: [{ src: "/images/EEEC1822.jpeg", alt: "Predaj a výmena prevádzkových kvapalín" }],
-    unoptimized: true,
   },
   {
     title: "3D Geometria aj pre dodávky s najdlhším rázvorom do 3,5t",
     description: "Ponúkame profesionálne nastavenie geometrie kolies pomocou najmodernejšej 3D technológie – a to nielen pre osobné vozidlá, ale aj pre dodávky s predĺženým rázvorom až do 3,5 tony. Presné nastavenie geometrie zabezpečuje rovnomerné opotrebenie pneumatík, lepšiu ovládateľnosť vozidla a bezpečnejšiu jazdu. Vďaka nášmu vybaveniu dokážeme obslúžiť aj väčšie úžitkové vozidlá, ktoré bežne vyžadujú špecializované servisné zázemie.",
     images: [{ src: "/images/7DA261F1.jpeg", alt: "3D geometria" }],
-    unoptimized: true,
   },
 ];
 
